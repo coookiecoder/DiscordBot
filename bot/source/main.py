549d839d7cf4ac3fa15e0227c6e30880f3f020f6
coookiecoder <ambroise.bareux@gmail.com>
2025-04-21 18:04:51 +0200

    switched to / command

--- bot/source/main.py
+++ bot/source/main.py
@@ -1,25 +1,16 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
-
 import os
 
 import discord
 from discord.ext import commands
 
-description = "UwU"
-
-intents = discord.Intents.all()
-intents.members = True
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+bot = commands.Bot()
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.slash_command(name="ping", description="command to ping the bot")
+async def ping(interaction):
+    await interaction.response.send_message('pong')
 
 bot.run(os.environ.get('TOKEN', 'nope'))
